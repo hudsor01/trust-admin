126218777ade734537ec945e421f4c46772c1db9
hudsor01 <hudsor01@icloud.com>
2026-01-18 21:54:17 -0600

    fix: add Sentry onRouterTransitionStart export for navigation instrumentation

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -4,6 +4,9 @@
 
 import * as Sentry from '@sentry/nextjs'
 
+// Required for Next.js navigation instrumentation
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
+
 Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
